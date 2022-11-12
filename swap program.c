a519ffc0bcfbc4bcc4fded2cbd223febfbe3dabe
Amaayu <118004037+Amaayu@users.noreply.github.com>
2022-11-12 20:19:57 +0530

    Add files via upload

---
+++ swap program.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main()
+
+{
+     int a,b;
+
+    printf("enter number a=");
+    scanf("%d",&a);
+    printf("enter number b=");
+    scanf("%d",&b);
+    
+   a=a+b;
+   b=a-b;
+   a=a-b;
+    
+    printf("a=%d b=%d",a,b);
+
+
+    return 0;
+}
